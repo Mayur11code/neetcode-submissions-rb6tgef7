2e3e5e649894c3efea6cf4c54aa4569541a0f015
Mayur Nanda <217201393+Mayur11code@users.noreply.github.com>
2026-08-20 00:20:54 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int wn1= 1;
+        int wn2 = 1;
+        int wnt =0;
+
+        for(int i=1; i<n; i++){
+            wnt = wn1 + wn2;
+            wn2= wn1;
+            wn1 = wnt;
+        }
+
+        return wnt ==0 ? n : wnt;
+    }
+};
